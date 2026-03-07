4e7dc8a3cf9f28941f30f6b4b697452f26bdceef
AaradhyaDashore <aaradhyadashore784@gmail.com>
2026-03-08 00:37:21 +0530

    Rewriting the test.py

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import argparse
-
+from sklearn.metrics import f1_score
 from ann.neural_network import NeuralNetwork
 
 best_config= argparse.Namespace(
@@ -22,3 +22,14 @@ model = NeuralNetwork(best_config)
 weights = np.load("src/best_model.npy", allow_pickle=True).item()
 
 model.set_weights(weights)
+
+X_test = np.random.rand(100, 784)  # 100 samples, 784 features
+
+y_true = np.random.randint(0, 10, size=(100,))  # 100 samples, 10 classes (0-9)
+
+model.forward(X_test)
+y_pred = model.act_out
+
+y_pred_labels = np.argmax(y_pred, axis=1)
+
+print("F1 Score:", f1_score(y_true, y_pred_labels, average='macro'))
